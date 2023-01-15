b1b0ca9905d12cab6e4487602516ec28b6cd06ee
Pluvia <keystroke915@naver.com>
2023-01-16 03:50:54 +0900

    이미지 검색 로직 속도 개선 (CVSharp의 boxing 연산 우회하여 직접 계산)

--- SightHighLighter/ImageProcessor.cs
+++ SightHighLighter/ImageProcessor.cs
@@ -2,37 +2,36 @@
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Interop;
 using System.Windows.Media.Imaging;
 using System.Windows.Media;
+using System.Windows.Media.Media3D;
 using OpenCvSharp;
 
 namespace SightHighlighter
 {
     public static class ImageProcessor
     {
-        private static readonly int primaryScreenLeft = 0;
-        private static readonly int primaryScreenTop = 0;
-        public static Mat imgtemp = new Mat();
-        private static double _threshold = 0.99;
-        public static double threshold
-        {
-            get { return _threshold; }
-            set { _threshold = value; }
-        }
-        public static readonly int matchCountThreshold = 10;
+        private static int _screenHeight = (int)SystemParameters.PrimaryScreenHeight;
+        private static int _screenWidth = (int)SystemParameters.PrimaryScreenWidth;
+
+        private const int PrimaryScreenLeft = 0;
+        private const int PrimaryScreenTop = 0;
+        public static Mat ImgTemp = new();
+        public static double Threshold { get; set; } = 0.99;
+
+        public static readonly int MatchCountThreshold = 10;
+        private static readonly System.Drawing.Pen _redPen = new(System.Drawing.Brushes.Red, 5);
 
 
-        private static System.Drawing.Pen redPen = new System.Drawing.Pen(System.Drawing.Brushes.Red, 5);
         public static Bitmap CaptureScreen() // ref: https://stackoverflow.com/questions/4978157/how-to-search-for-an-image-on-screen-in-c
         {
             var image = new System.Drawing.Bitmap((int)SystemParameters.PrimaryScreenWidth, (int)SystemParameters.PrimaryScreenHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
-            using (Graphics graphics = Graphics.FromImage(image))
-            {   // http://m.csharpstudy.com/Tip/View?aspx=Tip-screen-copy.aspx&title=C%23%EC%97%90%EC%84%9C%20%EC%8A%A4%ED%81%AC%EB%A6%B0%20%EC%BA%A1%EC%B3%90
-                graphics.CopyFromScreen(primaryScreenLeft, primaryScreenTop, 0, 0, image.Size, CopyPixelOperation.SourceCopy);
-            }
+            using Graphics graphics = Graphics.FromImage(image);
+            graphics.CopyFromScreen(PrimaryScreenLeft, PrimaryScreenTop, 0, 0, image.Size, CopyPixelOperation.SourceCopy);
             return image;
         }
 
@@ -88,79 +87,79 @@ namespace SightHighlighter
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool DeleteObject([In] IntPtr hObject);
 
-        public static ImageSource ImageSourceForBitmap(Bitmap bmp)
+        public static ImageSource? ImageSourceForBitmap(Bitmap bmp)
         {
             var handle = bmp.GetHbitmap();
             try
             {
-                ImageSource newSource = Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                ImageSource newSource = Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
 
-                DeleteObject(handle);
                 return newSource;
             }
             catch (Exception ex)
             {
-                DeleteObject(handle);
                 return null;
+            }
+            finally
+            {
+                DeleteObject(handle);
             }
         }
 
-        public static (int,ImageSource) FindImage()
+        public static unsafe (int, ImageSource) FindImage()
         {
             int matchCount = 0;
 
             Bitmap img = CaptureScreen();
-            Graphics graphics = Graphics.FromImage(img); // disposing this is important (memory leak)
+            using Graphics graphics = Graphics.FromImage(img); // disposing this is important (memory leak)
 
-            // img to mat using opencvsharp
-            // ref: https://kanais2.tistory.com/307
             Mat mimg = OpenCvSharp.Extensions.BitmapConverter.ToMat(img);
 
-            // convert matrix without memory violation
-            // ref: https://answers.opencv.org/question/4811/convertto-from-cv_32f-to-cv_8u/
-            // Todo: pull out conversion process from event loop and reduce duplication
-            Mat imgtemp_8UC3 = new Mat();
-            imgtemp.ConvertTo(imgtemp_8UC3, MatType.CV_8UC3);
-            Mat imgtemp_8UC4 = new Mat();
+            using Mat imgtemp_8UC3 = new Mat();
+            ImgTemp.ConvertTo(imgtemp_8UC3, MatType.CV_8UC3);
+
+            using Mat imgtemp_8UC4 = new Mat();
             Cv2.CvtColor(imgtemp_8UC3, imgtemp_8UC4, ColorConversionCodes.BGR2BGRA);
             imgtemp_8UC3.Dispose();
 
-            // template matching to result
-            Mat result = new Mat();
+            using Mat result = new Mat();
             Cv2.MatchTemplate(mimg, imgtemp_8UC4, result, TemplateMatchModes.CCorrNormed);
-            mimg.Dispose();
-            imgtemp_8UC4.Dispose();
 
-            // loop to match result
-            // Type specific Mat method
-            // ref: https://github.com/shimat/opencvsharp/wiki/Accessing-Pixel
-            var mat3 = new Mat<float>(result);
-            var indexer = mat3.GetIndexer();
-            for (int y = 0; y < result.Height; y++)
+
+            using var mat3 = new Mat<float>(result);
+            var p = (nint)mat3.DataPointer;
+            var spant = new Span<float>(mat3.DataPointer, mat3.Width * mat3.Height);
+
+            for (int y = 0; y < mat3.Height; y++)
             {
-                for (int x = 0; x < result.Width; x++)
+                for (int x = 0; x < mat3.Width; x++)
                 {
-                    if (matchCount >= matchCountThreshold)
+                    if (matchCount >= MatchCountThreshold)
                     {
                         goto PixelLoopEnd;
                     }
-                    double value = indexer[y, x];
-                    if (value >= _threshold)
+
+                    float value = Indexer(p, x, y, mat3.Width);
+                    //float value = spant[mat3.Width * y + x];
+                    if (value >= Threshold)
                     {
-                        Rectangle rect = new Rectangle(x, y, imgtemp.Width, imgtemp.Height);
-                        graphics.DrawRectangle(redPen, rect);
+                        var rect = new Rectangle(x, y, ImgTemp.Width, ImgTemp.Height);
+                        graphics.DrawRectangle(_redPen, rect);
                         matchCount++;
-                        //boxpoints.Add((x,y));
                     }
                 }
             }
-            PixelLoopEnd:
-            mat3.Dispose();
-            result.Dispose();
 
-            graphics.Dispose();
+            PixelLoopEnd:
 
             return (matchCount, ImageSourceForBitmap(img));
         }
+
+        private static unsafe float Indexer(nint p, int x, int y, int width)
+        {
+            return *(float*)(p + width * sizeof(float) * (long)y + sizeof(float) * (long)x);
+        }
+
     }
 }
